66f4dc7a39c3bd102d790763fd77438bed85cc04
mwhapeles <114778814+mwhapeles@users.noreply.github.com>
2024-08-23 00:36:17 -0600

    confetti?

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 // Check JS is Connected
 //console.log('👍 JS Connected');
-//import { Confetti } from "https://confettijs.org/confetti.min.js";
-
-//let confetti = new Confetti("confetti");
 
 // import the library
-
+let confetti = new Confetti("confetti");
 // get all the card
 document.querySelectorAll('.card').forEach( item => {
     
     // for each item i.e. card, do this
     item.addEventListener('click', event => {
-      
+      confetti = new Confetti("confetti"); 
+
       // toggle class on card
       item.classList.toggle('flipped');
     })
